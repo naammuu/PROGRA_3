6f02f91601660db88e12bb0cf9e1ebe21fad32af
naammuu <131481047+naammuu@users.noreply.github.com>
2023-04-22 00:59:27 -0600

    cambios

--- PROYECTO/PROYECTO/frmtipos.vb
+++ PROYECTO/PROYECTO/frmtipos.vb
@@ -2,4 +2,8 @@
     Private Sub lblCodigo_Click(sender As Object, e As EventArgs) Handles lblCodigo.Click
 
     End Sub
+
+    Private Sub btnIngresar_Click(sender As Object, e As EventArgs) Handles btnIngresar.Click
+
+    End Sub
 End Class
